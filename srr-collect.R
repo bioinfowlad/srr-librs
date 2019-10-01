e7af64e826c1a5dedd679c83aaf95d62358a9f5f
Wladimir Labeikovsky <wladimir.labeikovsky@cuanschutz.edu>
2019-10-01 09:42:05 -0600

    Added record collection code on its own script.

---
+++ srr-collect.R
@@ -0,0 +1,11 @@
+# srr-collect
+# downloads and collects records for systematic and other reviews
+# with authors from Anschutz from 2014-2018
+# script is part of "srr" project
+
+
+# Initialize
+library(tidyverse)
+library(easyPubMed)
+library(readxl)
+library(tidyxl)
