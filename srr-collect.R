7f995d1afd42fae6be9c3b4994b7d55d6f235a35
Wladimir Labeikovsky <wladimir.labeikovsky@cuanschutz.edu>
2019-10-01 16:26:49 -0600

    Updating preamble of srr-collect

--- srr-collect.R
+++ srr-collect.R
@@ -1,7 +1,11 @@
 # srr-collect
+#
 # downloads and collects records for systematic and other reviews
 # with authors from Anschutz from 2014-2018
-# script is part of "srr" project
+#
+# script is part of:
+# A Bibliographic Analysis of librarian assistance on SRs at CUAnschutz
+# (continuation of Craven/Palmer/Piper project)
 
 
 # Initialize
